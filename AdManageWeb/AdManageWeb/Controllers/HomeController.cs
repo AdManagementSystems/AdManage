78459cc822d7ae479dbc07f623b7ee164ba31057
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2024-03-17 00:04:12 +0300

    user page added

--- AdManageWeb/AdManageWeb/Controllers/HomeController.cs
+++ AdManageWeb/AdManageWeb/Controllers/HomeController.cs
@@ -25,6 +25,10 @@ namespace AdManageWeb.Controllers
         {
             return View();
         }
+        public IActionResult TestClient()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
 		{

b7e0f9ee123dd9826f9fa108443de66244a87193
yusuf <arasy541@gmail.com>
2024-03-17 20:05:36 +0300

    update default

--- AdManageWeb/AdManageWeb/Controllers/DefaultController.cs
+++ AdManageWeb/AdManageWeb/Controllers/DefaultController.cs
@@ -2,11 +2,11 @@
 
 namespace AdManageWeb.Controllers
 {
-	public class DefaultController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
 }

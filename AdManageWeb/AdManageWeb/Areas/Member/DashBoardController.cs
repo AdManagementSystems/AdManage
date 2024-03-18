f3c5c667570bd6b091266c7e2729e0bd2a7afe42
yusuf <arasy541@gmail.com>
2024-03-18 23:45:55 +0300

    member page added

---
+++ AdManageWeb/AdManageWeb/Areas/Member/DashBoardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdManageWeb.Areas.Member
+{
+    [Area("Member")]
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

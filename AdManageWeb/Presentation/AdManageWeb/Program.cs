c13d8934f4ab178820ddff3e1e103246a6230ac5
Furkan Kümüş <furkankumus1905@gmail.com>
2024-04-29 14:19:34 +0300

    Update Program.cs

--- AdManageWeb/Presentation/AdManageWeb/Program.cs
+++ AdManageWeb/Presentation/AdManageWeb/Program.cs
@@ -1,10 +1,10 @@
+using AdManageWeb.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using AdManage.Application.Features.CQRS.Handlers;
 using AdManage.Application.Interfaces;
 using AdManage.Persistence.DbContexts;
 using AdManage.Persistence.Repositories;
-using AdManageWeb.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 namespace AdManageWeb
 {
@@ -13,6 +13,16 @@ namespace AdManageWeb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
 
             builder.Services.AddHttpClient();
 
@@ -46,7 +56,11 @@ namespace AdManageWeb
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -65,19 +79,20 @@ namespace AdManageWeb
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
-
             app.MapControllerRoute(
                 name: "areas",
-                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-            );
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                  name: "areas",
+                  name: "admin",
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+
+            app.MapRazorPages();
 
             app.Run();
         }

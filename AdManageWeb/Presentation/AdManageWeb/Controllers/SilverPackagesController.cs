f3960dd4005d8874fd8c62230b5955cec7373bd6
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2024-05-03 22:08:25 +0300

    Packages.cshtml background update

--- AdManageWeb/Presentation/AdManageWeb/Controllers/SilverPackagesController.cs
+++ AdManageWeb/Presentation/AdManageWeb/Controllers/SilverPackagesController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdManage.Application.Features.CQRS.Handlers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AdManageWeb.Controllers
 {
     public class SilverPackagesController : Controller
     {
-        public IActionResult Index()
+        private readonly CreateSilverCommandHandler _createSilverCommandHandler;
+        private readonly GetSilverByIdQueryHandler _getSilverByIdQueryHandler;
+        private readonly GetSilverQueryHandler _getSilverQueryHandler;
+        private readonly UpdateSilverCommandHandler _updateSilverCommandHandler;
+        private readonly RemoveSilverCommandHandler _removeSilverCommandHandler;
+
+        public SilverPackagesController(CreateSilverCommandHandler createSilverCommandHandler, GetSilverByIdQueryHandler getSilverByIdQueryHandler, GetSilverQueryHandler getSilverQueryHandler, UpdateSilverCommandHandler updateSilverCommandHandler, RemoveSilverCommandHandler removeSilverCommandHandler)
         {
-            return View();
+            _createSilverCommandHandler = createSilverCommandHandler;
+            _getSilverByIdQueryHandler = getSilverByIdQueryHandler;
+            _getSilverQueryHandler = getSilverQueryHandler;
+            _updateSilverCommandHandler = updateSilverCommandHandler;
+            _removeSilverCommandHandler = removeSilverCommandHandler;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var SilverPackages = await _getSilverQueryHandler.Handle();
+            var SilverPackagesList = SilverPackages.Select(result => new AdManage.Domain.Entities.SilverPackages
+            {
+                Id = result.Id,
+                Description = result.Description,
+                Price = result.Price,
+                Image = result.Image,
+                CoverImage = result.CoverImage,
+                Details1 = result.Details1,
+                Details2 = result.Details2,
+                Image2 = result.Image2
+            }).ToList();
+
+            return View(SilverPackagesList);
         }
     }
 }
+

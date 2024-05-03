2ece922ce0153502ee1e2106fe50cb5c6ba4c804
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2024-05-03 20:57:31 +0300

    Gold packages added

--- AdManageWeb/Presentation/AdManageWeb/Controllers/GoldPackagesController.cs
+++ AdManageWeb/Presentation/AdManageWeb/Controllers/GoldPackagesController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdManage.Application.Features.CQRS.Handlers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AdManageWeb.Controllers
 {
     public class GoldPackagesController : Controller
     {
-        public IActionResult PackagesGold()
+        private readonly CreateGoldCommandHandler _createGoldCommandHandler;
+        private readonly GetGoldByIdQueryHandler _getGoldByIdQueryHandler;
+        private readonly GetGoldQueryHandler _getGoldQueryHandler;
+        private readonly UpdateGoldCommandHandler _updateGoldCommandHandler;
+        private readonly RemoveGoldCommandHandler _removeGoldCommandHandler;
+
+        public GoldPackagesController(CreateGoldCommandHandler createGoldCommandHandler, GetGoldByIdQueryHandler getGoldByIdQueryHandler, GetGoldQueryHandler getGoldQueryHandler, UpdateGoldCommandHandler updateGoldCommandHandler, RemoveGoldCommandHandler removeBronzeCommandHandler)
         {
-            return View();
+            _createGoldCommandHandler = createGoldCommandHandler;
+            _getGoldByIdQueryHandler = getGoldByIdQueryHandler;
+            _getGoldQueryHandler = getGoldQueryHandler;
+            _updateGoldCommandHandler = updateGoldCommandHandler;
+            _removeGoldCommandHandler = removeBronzeCommandHandler;
+        }
+
+        public async Task<IActionResult> PackagesGold()
+        {
+            var goldPackages = await _getGoldQueryHandler.Handle();
+            var goldPackagesList = goldPackages.Select(result => new AdManage.Domain.Entities.GoldPackages
+            {
+                Id = result.Id,
+                Description = result.Description,
+                Price = result.Price,
+                Image = result.Image,
+                CoverImage = result.CoverImage,
+                Details1 = result.Details1,
+                Details2 = result.Details2,
+                Image2 = result.Image2
+            }).ToList();
+
+            return View(goldPackagesList);
         }
     }
 }
+
